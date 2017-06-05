236e6124b4ae61220b5f9fbcd06105092e0e3e85
Jared Sprague <jared@caramelcode.com>
2017-06-05 11:22:25 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var StatuspageController = require('statuspage-controller');
+var IrcPlugin = require('statuspage-controller-irc');
+
+var plugin_config = {
+    host: 'irc.devel.redhat.com',
+    nick: 'Statuspage',
+    prefix: '[status.redhat.com]',
+    channels: [
+        '#customer-platform',
+        '#iso',
+        '#MIM',
+        '#cee',
+        '#cee_mgrs',
+        '#gss-na',
+        '#it-pnt'
+    ]
+};
+
+var spc = new StatuspageController();
+
+spc.addPlugin(new IrcPlugin(plugin_config));
+
+spc.start();
