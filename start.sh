d45cc8d84b659f1d032d6e93ce478d11c208c857
Jared Sprague <jared@caramelcode.com>
2017-06-13 10:48:38 -0400

    Added new relic apm agent

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Copy the newrelic.js file to root of application
+cp node_modules/newrelic/newrelic.js ./
+
 # Replace new relic agent applicatio name and license key
-sed -i "s/My Application/Statuspage Controller/g" node_modules/newrelic/newrelic.js
-sed -i "s/license key here/b2d03d9ffe7424cbaf5ea5a81ab372bf3f2d909e/g" node_modules/newrelic/newrelic.js
+sed -i "s/My Application/Statuspage Controller/g" ./newrelic.js
+sed -i "s/license key here/b2d03d9ffe7424cbaf5ea5a81ab372bf3f2d909e/g" ./newrelic.js
